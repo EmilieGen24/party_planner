74833e6bac9a05f953ecec450e77179624abae1f
Emilie Geneste <mimigeneste@gmail.com>
2026-02-23 11:45:52 +0100

    [modif]style_comment_btn_alertmessage

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -6,6 +6,7 @@ use App\Entity\Comment;
 use App\Entity\Theme;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +15,18 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('comment_text')
-            ->add('theme', EntityType::class, [
-                'class' => Theme::class,
-                'choice_label' => 'id',
+            ->add('comment_text', TextareaType::class, [
+                'label' => 'Votre commentaire : ',
+                'attr' => [
+                    'rows' => 6,
+                    'placeholder' => 'Écrivez votre commentaire ici...',
+                    'class'=> 'commentaire-area'
+                ]
             ])
+            // ->add('theme', EntityType::class, [
+            //     'class' => Theme::class,
+            //     'choice_label' => 'id',
+            // ])
         ;
     }
 
